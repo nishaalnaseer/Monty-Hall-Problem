5a9869d7ff81b5d8eaad0e9d098ca94ec220ee0b
nishaalnaseer <nishawl.naseer@outlook.com>
2021-07-11 20:07:24 +0500

    Turned it into a class
    You can now have an easier time getting the data with the crux() function as the whole program is now a class. Also imported only shuffle() and randint() functions from random

--- monty_hall.py
+++ monty_hall.py
@@ -6,97 +6,105 @@ with a goats behind. The host always opens the door of a goat.
 this code prints out the amount of times, for the amount of times iterated,
 you would get cars or goats by chosing to swap or not"""
 
-import random
+from random import shuffle, randint
 
-def selectDoor():
-    """initialise the starting of the game by shuffling the 'doors' and picking
-    a choice there at random... i guess line 9 makes this a lot more random"""
+class MontyHallProb():
+    """docstring for MontyHallProb"""
+    def __init__(self, number, swap):
+        self.number =  number
+        self.swap = swap
 
-    choices = ['goat', 'goat', 'car'] # the door beind each as such
-    random.shuffle(choices) # shuffle the doors
+    def selectDoor(self):
+        """initialise the starting of the game by shuffling the 'doors' and picking
+        a choice there at random... i guess line 9 makes this a lot more random"""
 
-    myChoiceInd = random.randint(0, 2) # pick a random int(0 to 2)
+        choices = ['goat', 'goat', 'car'] # the door beind each as such
+        shuffle(choices) # shuffle the doors
 
-    myChoice = choices[myChoiceInd] # pick a door with the random int
+        myChoiceInd = randint(0, 2) # pick a random int(0 to 2)
 
-    del choices[myChoiceInd] # remove the selected element, it already in myChoice
+        myChoice = choices[myChoiceInd] # pick a door with the random int
 
-    return([myChoice, choices])
+        del choices[myChoiceInd] # remove the selected element, it already in myChoice
 
-def openDoor(choices):
-    """Simulates the idea of the host opening the door containing a goats
-    and removing it from the list or doors"""
-    choices.remove('goat')
-    return(choices)
+        return([myChoice, choices])
 
-def increments(myChoice, cars, goats):
-    """cars and goats variables are lists and this function increments them each 
-    time, the else statement of the if block is for debuggin"""
-    if myChoice == 'car':
-        cars += 1
-    elif myChoice == 'goat':
-        goats += 1
-    else:
-        print("Error at increment()")
+    def openDoor(self, choices):
+        """Simulates the idea of the host opening the door containing a goats
+        and removing it from the list or doors"""
+        choices.remove('goat')
+        return(choices)
 
-    return([cars, goats])
+    def increments(self, myChoice, cars, goats):
+        """cars and goats variables are lists and this function increments them each 
+        time, the else statement of the if block is for debuggin"""
+        if myChoice == 'car':
+            cars += 1
+        elif myChoice == 'goat':
+            goats += 1
+        else:
+            print("Error at increment()")
 
-def swapFunc(myChoice, choices):
-    """simulates the idea of the contestant swapping. there is only one element
-    in the list and this function returns that choice
-    if you print out the list of choices at this stage you would realise there
-    is only one element, because
-    1. after the contestant chose a door that door should no longer be counted 
-    and hence it was removed
-    2. after the host opened the door with a goats, the first goats in the list
-    was removed... although it was the first goats it is still random becuase 
-    the list was shuffled"""
+        return([cars, goats])
 
-    myChoice = choices[0]
-    return(myChoice)
+    def swapFunc(self, myChoice, choices):
+        """simulates the idea of the contestant swapping. there is only one element
+        in the list and this function returns that choice
+        if you print out the list of choices at this stage you would realise there
+        is only one element, because
+        1. after the contestant chose a door that door should no longer be counted 
+        and hence it was removed
+        2. after the host opened the door with a goats, the first goats in the list
+        was removed... although it was the first goats it is still random becuase 
+        the list was shuffled"""
 
-def crux(number, swap):
-    """as the name of the function this is the crux of the matter where
-    we loop for the number of times we want to carsry this out"""
+        myChoice = choices[0]
+        return(myChoice)
 
-    cars, goats = 0, 0
+    def crux(self):
+        """as the name of the function this is the crux of the matter where
+        we loop for the number of times we want to carsry this out"""
 
-    for num in range(number):
-        initialised = selectDoor() # init and select a door
-        myChoice = initialised[0] #get your choice
-        choices = initialised[1] # get your init list
+        cars, goats = 0, 0
 
-        choices = openDoor(choices) # get a goats removed
+        for num in range(self.number):
+            initialised = self.selectDoor() # init and select a door
+            myChoice = initialised[0] #get your choice
+            choices = initialised[1] # get your init list
 
-        if swap == True:
-            """if the contestant wants to swap"""
-            myChoice = swapFunc(myChoice, choices)
+            choices = self.openDoor(choices) # get a goats removed
 
-        incremented = increments(myChoice, cars, goats)
-        cars = incremented[0]
-        goats = incremented[1]
+            if self.swap == True:
+                """if the contestant wants to swap"""
+                myChoice = self.swapFunc(myChoice, choices)
 
-    return([cars, goats])
+            incremented = self.increments(myChoice, cars, goats)
+            cars = incremented[0]
+            goats = incremented[1]
 
-def mainOutput(number, swap):
-    """the main procesess are done in crux() this function is to format and 
-    output the data
-    number is the operation count and swap is if you want to swap the door"""
-    stats = crux(number, swap) # get the data
+        return([cars, goats])
 
-    if swap == False:
-        # binary statement is for the output to make sense
-        binary_statment = "with no swaps"
-    else:
-        binary_statment = "with swaps"
+    def printStats(self):
+        """the main procesess are done in crux() this function is to format and 
+        output the data
+        number is the operation count and swap is if you want to swap the door"""
+        stats = self.crux() # get the data
 
-    cars = stats[0]
-    goats = stats[1]
+        if self.swap == False:
+            # binary statement is for the output to make sense
+            binary_statment = "with no swaps"
+        else:
+            binary_statment = "with swaps"
 
-    printStatement = f"Out of {number} times, {binary_statment}, cars came out "
-    printStatement += f"{cars} times and goats came out {goats} times."
+        cars = stats[0]
+        goats = stats[1]
 
-    print(printStatement)
+        printState = f"Out of {self.number} times, {binary_statment}, cars came"
+        printState += f" out {cars} times and goats came out {goats} times."
+
+        print(printState)
 
 if __name__ == '__main__':
-    mainOutput(number=1000000, swap=False)
+
+    round1 = MontyHallProb(1000000, True)
+    round1.printStats()
